c1dd515b280e343d4e3bb63175aa850413083227
Thaz <76603398+thasinaz@users.noreply.github.com>
2021-04-21 10:38:28 +0800

    Add check-xor check-fletcher

---
+++ file-integrity/check-fletcher.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define INIT_S1 0
+#define INIT_S2 0
+
+int main(int argc, char *argv[]) {
+    FILE *fd;
+    switch (argc) {
+    case 1:
+        fd = stdin;
+        break;
+    case 2:
+        if ((fd = fopen(argv[1], "r")) == NULL) {
+            perror("fopen");
+            return -1;
+        }
+        break;
+    default:
+        fprintf(stderr, "Usage: %s [pathname]\n", argv[0]);
+        return -1;
+    }
+
+    unsigned char s1 = INIT_S1,
+                  s2 = INIT_S2;
+
+    unsigned char d;
+    while (fread(&d, 1, 1, fd) == 1) {
+        s1 = (s1 + d) % 255;
+        s2 = (s2 + s1) % 255;
+    }
+    if (ferror(fd)) {
+        perror("fread");
+        return -1;
+    }
+
+    unsigned short rc = ((short) s1 << 8) + (short) s2;
+    fwrite(&rc, 2, 1, stdout);
+    return 0;
+}
---
+++ file-integrity/check-xor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define INIT 0
+
+int main(int argc, char *argv[]) {
+    FILE *fd;
+    switch (argc) {
+    case 1:
+        fd = stdin;
+        break;
+    case 2:
+        if ((fd = fopen(argv[1], "r")) == NULL) {
+            perror("fopen");
+            return -1;
+        }
+        break;
+    default:
+        fprintf(stderr, "Usage: %s [pathname]\n", argv[0]);
+        return -1;
+    }
+
+    unsigned char rc = INIT;
+    unsigned char d;
+    while (fread(&d, 1, 1, fd) == 1) {
+        rc ^= d;
+    }
+    if (ferror(fd)) {
+        perror("fread");
+        return -1;
+    }
+
+    fwrite(&rc, 1, 1, stdout);
+    return 0;
+}
